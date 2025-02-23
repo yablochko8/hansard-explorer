3ff43ed6cef5a6a2d0d399d356dd911ed78be056
Lui <6627294+yablochko8@users.noreply.github.com>
2025-02-23 13:35:48 +0000

    .

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,9 @@ import { DatePicker } from './components/DatePicker'
 function App() {
 
   const [selectedDate, setSelectedDate] = useState<Date>(new Date('2002-04-16'))
+
+  const [sittingDate, setSittingDate] = useState<Date | null>(null)
+
   const [sitting, setSitting] = useState<any>(null)
 
   const handleSubmit = () => {
@@ -15,6 +18,7 @@ function App() {
     if (selectedDate) {
       getSitting(selectedDate).then((sitting) => {
         setSitting(sitting);
+        setSittingDate(selectedDate);
       });
     }
     else {
@@ -24,13 +28,13 @@ function App() {
 
   console.log("selectedDate", selectedDate)
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
+    <div className="flex flex-col items-center justify-start h-screen">
       <DatePicker selectedDate={selectedDate} onDateChange={setSelectedDate} />
-      <button onClick={handleSubmit}>Submit</button>
+      <button onClick={handleSubmit}>Fetch</button>
 
       {sitting && (
         <div>
-          Sitting found with length {sitting.length}
+          Sitting (for {sittingDate?.toLocaleDateString()}) found with length {sitting.length}
         </div>
       )}
     </div>
